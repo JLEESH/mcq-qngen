68373324e0de217979b1bbc1b0ff07c28c95faff
JLEESH <jimmyleesh@yahoo.co.kr>
2025-01-08 19:47:04 +0900

    updated qngen.py to handle custom paths.

--- TODO.md
+++ TODO.md
@@ -3,8 +3,7 @@
 ## Misc.
 cf. Source files.
 
-
-## Streamline the Process
+## Ease of Use
 Maybe make it such that the user can
 
 1. run ``qngen.py`` (for a new set of questions, formatted and placed into the right path)
@@ -18,3 +17,13 @@ We could combine both files or make a mini-library, but that would be for the it
 
 ## Web Version
 (hmm..., simpe node server -> host a html with a js script to read and display generated questions? dynamically generate questions? -> need to keep track of cost and abuse, or pre-generate a number of questions and segment into short batches -> selection menu, etc.)
+
+## Question Quality
+Improve the quality of questions.
+
+Some issues:
+1. Shuffle the options manually as the API responses have an overwhelmingly larger share of 'A' being the answer.
+2. Repetition of questions - Flexibility in not repeating questions that have already been generated.
+3. Question types - increase the diversity of question types.
+4. TBD
+
--- qngen.py
+++ qngen.py
@@ -2,7 +2,10 @@ from openai import OpenAI
 
 oa_key = None # replace with caution
 
-PROTECT_QN_FILE = True
+PROTECT_QN_FILE = True # may be modified by main()
+DUMMY_RESPONSE_PATH = "sample_responses/dummy_response"
+DEFAULT_RESP_PATH = "./" # not used
+DEFAULT_QN_PATH = "./" # not used
 
 # edit prompt as needed.
 QNGEN_SYSTEM_PROMPT = """
@@ -10,15 +13,15 @@ QNGEN_SYSTEM_PROMPT = """
     Each question must adhere to the following JSON format:
     
     {
-        \"id\": "e2212784-4dea-454b-ac23-442e815c218f",
-        \"_comments\": ["internal-index: 0"],
+        \"id\": "e2212784-4dea-454b-ac23-442e815c218f",     // UUID
+        \"_comments\": ["internal-index: 0"],               // Internal index and other miscellaneous comments
         \"question-type\": \"simple\",
         \"question\": [\"Question word\"],
         \"options\": [\"Option 1\", \"Option 2\", \"Option 3\", \"Option 4\"],
         \"options-alt-list\": [[\"Alt 1\", \"Alt 2\", \"Alt 3\", \"Alt 4\"],
                                 [\"Alt-2 1\", \"Alt-2 2\", \"Alt-2 3\", \"Alt-2 4\"]],
-        \"answer\": 0,  # Index of correct answer
-        \"answer-alt\": [0, 0]  # Index of correct answer in alternative lists
+        \"answer\": 0,                                      //Index of correct answer
+        \"answer-alt\": [0, 0]                              // Index of correct answer in alternative lists
     }
 
     Instructions:
@@ -26,7 +29,7 @@ QNGEN_SYSTEM_PROMPT = """
     - Use a mix of questions of simple and intermediate difficulty e.g. vocabulary of different levels.
     - Provide plausible distractors for all answer options.
     - Ensure high-quality, educationally sound options.
-    - Ensure an even mixture of the indices of correct answers.
+    - Ensure an even mixture of the answer indices.
 
     Example Question for a Vienamese vocabulary quiz:
     {
@@ -40,15 +43,25 @@ QNGEN_SYSTEM_PROMPT = """
         \"answer\": 1,
         \"answer-alt\": [1, 1]
     }
-    "        "Generate output as a valid JSON structure."
+
+    Generate output as a valid JSON structure.
 """
+
 # edit prompt to change number of questions etc.
 nQuestions = 10
-QNGEN_USER_PROMPT = f"{nQuestions} question of Vietnamese. \
-    Easy native Vietnamese words and intermediate to advanced \
-    Sino-Vietnamese words for learners with a background in both English, Chinese and Japanese."
+QNGEN_USER_PROMPT = f"{nQuestions} questions of Vietnamese. "\
+    "Easy native Vietnamese words and intermediate to advanced Sino-Vietnamese words "\
+    "for learners with a background in both English, Chinese and Japanese."
 
-def generate_response(system_text, user_text, oa_key=oa_key):
+def generate_questions(oa_key=oa_key, dummy=False):
+    if dummy:
+        return generate_response(QNGEN_SYSTEM_PROMPT, QNGEN_USER_PROMPT, oa_key=oa_key, dummy=True)
+    
+    return generate_response(QNGEN_SYSTEM_PROMPT, QNGEN_USER_PROMPT, oa_key=oa_key)
+
+def generate_response(system_text, user_text, oa_key=oa_key, dummy=False):
+    if dummy:
+        return load_response_var(DUMMY_RESPONSE_PATH)
     if oa_key == None:
         raise ValueError("OpenAI API key is not set.")
     
@@ -96,23 +109,25 @@ def save_questions(response, id=None, filename=None):
         else:
             filename = f"questions_{id}.json"
     
-    filecontent = response.choices[0].message.content[8:-3]
+    filecontent = response.choices[0].message.content[8:-3] # remove "```json\n" and "```" from the response
 
     with open(filename, "w") as f:
         f.write(filecontent)
 
-    # save questions to a set file as well
-    # check if file already exists
+    # save questions to ``questions.json``
     import os
     if os.path.exists("questions.json") and PROTECT_QN_FILE == True:
         # raise FileExistsError("WARNING: -d was not passed (questions.json is protected) \
         #     and ``questions.json`` already exists. \
         #     Please move or delete the file.\n\
         #     Continuing without overwriting... (``questions_<UUID>.json`` is still saved.)"
-        print("WARNING: -d was not passed (questions.json is protected) and ``questions.json`` already exists.\nPlease move or delete the file.\nContinuing without overwriting... (``questions_<UUID>.json`` is still saved.)")
 
-        with open("questions.json", "w") as f:
-            f.write(filecontent)
+        # print warning instead of raising an error, for now
+        print("WARNING: -d was not passed (questions.json is protected) and ``questions.json`` already exists.\nPlease move or delete the file.\nContinuing without overwriting... (``questions_<UUID>.json`` is still saved.)")
+        return
+    
+    with open("questions.json", "w") as f:
+        f.write(filecontent)
 
 def save_response_text(response, id=None, filename=None):
     if filename == None:
@@ -159,30 +174,46 @@ def save_response_content_text(response, id=None, filename=None):
     with open(filename, "w") as f:
         f.write(response.choices[0].message.content)
 
-def generate_questions(oa_key=oa_key):
-    return generate_response(QNGEN_SYSTEM_PROMPT, QNGEN_USER_PROMPT, oa_key=oa_key)
-
 def main():
     # process args
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--do-not-protect-qn-file", "-d",
-                       dest="DNP",
-                       action="store_true",
-                       default=False,
-                       help="Allow ``questions.json`` to be overwritten.")
+                        dest="DNP",
+                        action="store_true",
+                        default=False,
+                        help="Allow ``questions.json`` to be overwritten.")
     parser.add_argument("--generated-response-save", "-g",
-                       dest="GEN_RES_SAVE",
-                       action="store_true",
-                       default=False,
-                       help="Save generated responses.")
+                        dest="GEN_RES_SAVE",
+                        action="store_true",
+                        default=False,
+                        help="Save generated responses.")
+    parser.add_argument("--path-question", "-q",
+                        dest="QN_PATH",
+                        type=str,
+                        default=DEFAULT_QN_PATH,
+                        help="Set path to save the questions to (unimplemented).")
+    parser.add_argument("--path-response", "-r",
+                        dest="RESP_PATH",
+                        type=str,
+                        default=DEFAULT_RESP_PATH,
+                        help="Set path to save the responses to (unimplemented).")
+    parser.add_argument("--dummy", "-D", action="store_true", default=False, help="Run in dummy mode.")
     args = parser.parse_args()
     global PROTECT_QN_FILE
     PROTECT_QN_FILE = args.DNP == False
     M_GEN_RES_SAVE = args.GEN_RES_SAVE
+    DUMMY = args.dummy
+    QN_PATH = args.QN_PATH.strip()
+    RESP_PATH = args.RESP_PATH.strip()
+    
+    import os
+    if os.path.exists(QN_PATH) == False:
+        os.makedirs(QN_PATH)
+    if os.path.exists(RESP_PATH) == False:
+        os.makedirs(RESP_PATH)
     
     # load .env file to obtain OpenAI API key
-    import os
     from dotenv import load_dotenv
 
     load_dotenv()
@@ -192,23 +223,25 @@ def main():
 
     # obtain response
     print("Attempting to obtain a response...")
-    response = generate_questions(oa_key)
+    response = generate_questions(oa_key, dummy=DUMMY)
     print("Respose obtained.")
 
     # save response to file
     import uuid
     file_uuid = str(uuid.uuid4())
-    filename_var = f"response_{file_uuid}.txt"
-    filename_text = f"response_text_{file_uuid}.txt"
-    filename_content_text = f"response_content_text_{file_uuid}.txt"
+    if DUMMY:
+        file_uuid = "dummy_" + file_uuid
+    filename_var = RESP_PATH + f"response_{file_uuid}.txt"
+    filename_text = RESP_PATH + f"response_text_{file_uuid}.txt"
+    filename_content_text = RESP_PATH + f"response_content_text_{file_uuid}.txt"
 
     if M_GEN_RES_SAVE:
-        save_response_var(response, id=file_uuid)
-        save_response_text(response, id=file_uuid)
-        save_response_content_text(response, id=file_uuid)
+        save_response_var(response, id=file_uuid, filename=filename_var)
+        save_response_text(response, id=file_uuid, filename=filename_text)
+        save_response_content_text(response, id=file_uuid, filename=filename_content_text)
 
     # save questions to file
-    save_questions(response, id=file_uuid)
+    save_questions(response, id=file_uuid, filename=QN_PATH + f"questions_{file_uuid}.json")
     
     return response
 
